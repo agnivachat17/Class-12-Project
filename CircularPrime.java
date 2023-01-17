ba7c36a6ceb323607cd1ce6c2f305e1806b94a52
Agniva <122819473+agnivachat17@users.noreply.github.com>
2023-01-17 20:25:20 +0530

    Update CircularPrime.java

--- CircularPrime.java
+++ CircularPrime.java
@@ -1,3 +1,19 @@
+/*
+A Circular Prime is a prime number that remains prime under cyclic shifts of its digits. When
+the leftmost digit is removed and replaced at the end of the remaining string of digits, the generated number is
+still prime. The process is repeated until the original number is reached again. A number is said to be prime if
+it has only two factors I and itself.
+Example: 131 311 113 Hence, 131 is a circular prime.
+Test your program with the sample data and some random data:
+Example 1
+INPUT :
+N = 197
+OUTPUT:
+197
+971
+719
+197 IS A CIRCULAR PRIME
+*/
 import java.util.*;
 public class CircularPrime
 {
