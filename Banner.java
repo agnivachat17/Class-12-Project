e90370a8c27fbe684d7a8f0db02f558f538cc740
Agniva <122819473+agnivachat17@users.noreply.github.com>
2023-01-17 20:31:29 +0530

    Update Banner.java

--- Banner.java
+++ Banner.java
@@ -1,3 +1,25 @@
+/*
+The names of the teams participating in a competition should be displayed on a banner
+vertically, to accommodate as many teams as possible in a single banner. Design a program to accept the names
+of N teams, where 2 &lt; N &lt; 9 and display them in vertical order, side by side with a horizontal tab (i.e. eight
+spaces). Test your program for the following data and some random data:
+Example 1:
+INPUT:
+N = 3
+Team 1: Emus
+Team 2: Road Rols
+Team 3: Coyote
+OUTPUT:
+E R C
+m o o
+u a y
+s d o
+    t
+  R e
+  o
+  l
+  s
+*/
 import java.util.*;
 class Banner
 {   
